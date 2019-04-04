bbdce9b8c6c7fd762f34e483d686c6d3f6444b91
Anton Siniorg <anton@speedupmate.com>
2019-04-04 19:24:39 +0300

    magento2-os-2.2.4-with-samples

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit9480e23a770f6e61775210d8ea5f2a5d::getLoader();
+return ComposerAutoloaderInit90627204a2ac23ab5183339dba839362::getLoader();
